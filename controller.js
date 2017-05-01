91bc0dd4a028a2c88d1a798af051c07ff1c5f37b
jasonvandewarker <jasonvandewarker@gmail.com>
2017-04-30 19:54:41 -0700

    Structure mostly set up.

---
+++ controller.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('vstda', vstda);
+
+    vstda.$inject = [''];
+
+    /* @ngInject */
+    function vstda() {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+
+        }
+    }
+})();

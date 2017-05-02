ee4725a80fb2f99907c26f56a42c2ce64c42e58a
jasonvandewarker <jasonvandewarker@gmail.com>
2017-05-02 16:43:47 -0700

    Finally working.

---
+++ app/vstda.controller.js
@@ -0,0 +1,24 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('VstdaController', VstdaController);
+
+    VstdaController.$inject = [];
+
+    /* @ngInject */
+    function VstdaController() {
+        var vm = this;
+        vm.title = 'VstdaController';
+
+        vm.toDoArray = [];
+        vm.postList = postList;
+
+        function postList() {
+            vm.toDoArray.push({ toDo: vm.toDoObject.toDoInput, priority: vm.toDoObject.priority });
+
+        }
+
+    }
+})();

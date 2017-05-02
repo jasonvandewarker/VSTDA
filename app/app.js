ed7c86f493a77680db9eea818a0b678fe972b712
jasonvandewarker <jasonvandewarker@gmail.com>
2017-05-01 17:30:53 -0700

    Almost works, not quite.

--- app/app.js
+++ app/app.js
@@ -2,7 +2,32 @@
     'use strict';
 
     angular
-        .module('app', [
-            'dependencies'
-        ]);
+        .module('app', []);
+})();
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('VstdaController', VstdaController);
+
+    VstdaController.$inject = [];
+
+    /* @ngInject */
+    function VstdaController() {
+        var vm = this;
+
+        vm.todDoArray = [];
+        vm.toDoObject = {};
+
+        vm.postList = function() {
+          vm.toDoArray.push(vm.toDoObject);
+          vm.toDoObject = ' ';
+          console.log(vm.toDoArray);
+        };
+
+
+
+    }
 })();

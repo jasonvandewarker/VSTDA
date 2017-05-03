c0c569b5fe2c32d23effa4861c45f95f3d9aabca
jasonvandewarker <jasonvandewarker@gmail.com>
2017-05-03 15:26:47 -0700

    Added comments.

--- app/vstda.controller.js
+++ app/vstda.controller.js
@@ -14,7 +14,8 @@
 
         vm.toDoArray = [];
         vm.postList = postList;
-
+// Takes the info put into the input and the priority set by the
+// select menu and pushes them both into an array.
         function postList() {
             vm.toDoArray.push({ toDo: vm.toDoObject.toDoInput, priority: vm.toDoObject.priority });
         }
